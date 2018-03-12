651158d9faa487c65eff4331cbf8d32567198b3f
Tennyson Holloway <tennyson.holloway@gmail.com>
2018-03-12 15:39:22 -0700

    comments

--- csvparse.js
+++ csvparse.js
@@ -1,25 +1,29 @@
 var csv = require('csv');
 var fs = require('fs');
 
-
+/* constants into csv */
 const ARTIST_IDX = 0;
 const SONG_IDX = 1;
 const RECORD_IDX = 2;
 const LABEL_IDX = 3;
-const COUNT_IDX = 4;
+const COUNT_IDX = 4; 
 
+
+/* process the songs and store them in this arr */
 let songs = [];
 
 var parser = csv.parse({delimiter: ','}, function(err, data){
 
-    console.log(data[0]);
-
+    /* test if two rows in csv file are the same. */
     let match = function(a, b) {
         return a[ARTIST_IDX].trim().toLowerCase() == b[ARTIST_IDX].trim().toLowerCase()
             //&& a[SONG_IDX].trim().toLowerCase() == b[SONG_IDX].trim().toLowerCase()
             && a[RECORD_IDX].trim().toLowerCase() == b[RECORD_IDX].trim().toLowerCase();
     }
 
+    /* iterate over all rows. for each entry, check if it matches any existing processed songs.
+     * if so, increase the count variable. if not, set count variable to one and add it to
+     * processed songs */
     data.forEach(entry => {
         let found = false;
         for (var i = 0; i < songs.length; i++) {
@@ -34,13 +38,17 @@ var parser = csv.parse({delimiter: ','}, function(err, data){
         }
     });
 
-
+    /* sort the array based on count idx */
     songs.sort((a, b) => {
         return b[COUNT_IDX] - a[COUNT_IDX];
     })
 
+    /* top 50 only */
     songs = songs.slice(0, 50);
 
+
+
+    /* string.format() from the internet */
     if (!String.prototype.format) {
       String.prototype.format = function() {
         var args = arguments;
@@ -53,6 +61,7 @@ var parser = csv.parse({delimiter: ','}, function(err, data){
       };
     }
 
+    /* display it */
     songs.forEach(song => {
         console.log("{0} - {1} by {2}. Album: {3} Label: {4}".format(song[COUNT_IDX], song[SONG_IDX], song[ARTIST_IDX], song[RECORD_IDX], song[LABEL_IDX]));
 
