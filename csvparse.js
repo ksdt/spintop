8903bace6d531d7e351759a60032bda9185ccdfd
Bijan Farahani <bijancfarahani@gmail.com>
2018-04-21 16:41:31 -0700

    fixed second time bug

--- csvparse.js
+++ csvparse.js
@@ -12,79 +12,79 @@ const COUNT_IDX = 4;
 /* process the songs and store them in this arr */
 let songs = [];
 var songsLog = '';
+var count = 0;
 
 var parser = csv.parse({delimiter: ','}, function(err, data){
+    count++;
+    console.log(count);
     songs = [];
     songsLog = '';
     try {
-    /* test if two rows in csv file are the same. */
-    let match = function(a, b) {
-        return a[ARTIST_IDX].trim().toLowerCase() == b[ARTIST_IDX].trim().toLowerCase()
-            //&& a[SONG_IDX].trim().toLowerCase() == b[SONG_IDX].trim().toLowerCase()
-            && a[RECORD_IDX].trim().toLowerCase() == b[RECORD_IDX].trim().toLowerCase();
-    }
+        /* test if two rows in csv file are the same. */
+        let match = function(a, b) {
+            return a[ARTIST_IDX].trim().toLowerCase() == b[ARTIST_IDX].trim().toLowerCase()
+                //&& a[SONG_IDX].trim().toLowerCase() == b[SONG_IDX].trim().toLowerCase()
+                && a[RECORD_IDX].trim().toLowerCase() == b[RECORD_IDX].trim().toLowerCase();
+        }
 
-    /* iterate over all rows. for each entry, check if it matches any existing processed songs.
-     * if so, increase the count variable. if not, set count variable to one and add it to
-     * processed songs */
-    
-            data.forEach(entry => {
-        let found = false;
-        for (var i = 0; i < songs.length; i++) {
-            if (match(songs[i], entry)) {
-                found = true;
-                songs[i][COUNT_IDX]++;
+        /* iterate over all rows. for each entry, check if it matches any existing processed songs.
+         * if so, increase the count variable. if not, set count variable to one and add it to
+         * processed songs */
+        
+                data.forEach(entry => {
+            let found = false;
+            for (var i = 0; i < songs.length; i++) {
+                if (match(songs[i], entry)) {
+                    found = true;
+                    songs[i][COUNT_IDX]++;
+                }
             }
-        }
-        if (!found) {
-            entry.push(1); /* count idx */
-            songs.push(entry)
-        }
-    });
-
-    /* sort the array based on count idx */
-    songs.sort((a, b) => {
-        return b[COUNT_IDX] - a[COUNT_IDX];
-    })
-
-    /* top 50 only */
-    songs = songs.slice(0, 50);
-
-
-
-    /* string.format() from the internet */
-    if (!String.prototype.format) {
-      String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) {
-          return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-          ;
+            if (!found) {
+                entry.push(1); /* count idx */
+                songs.push(entry)
+            }
         });
-      };
+
+        /* sort the array based on count idx */
+        songs.sort((a, b) => {
+            return b[COUNT_IDX] - a[COUNT_IDX];
+        })
+
+        /* top 50 only */
+        songs = songs.slice(0, 50);
+
+
+
+        /* string.format() from the internet */
+        if (!String.prototype.format) {
+          String.prototype.format = function() {
+            var args = arguments;
+            return this.replace(/{(\d+)}/g, function(match, number) {
+              return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+              ;
+            });
+          };
+        }
+
+        /* display it */ //send log back to server
+
+        songs.forEach(song => {
+            songsLog += "{0} - {1} by {2}. Album: {3} Label: {4}".format(song[COUNT_IDX], song[SONG_IDX], song[ARTIST_IDX], song[RECORD_IDX], song[LABEL_IDX]) + '<br>';
+            //console.log("{0} - {1} by {2}. Album: {3} Label: {4}".format(song[COUNT_IDX], song[SONG_IDX], song[ARTIST_IDX], song[RECORD_IDX], song[LABEL_IDX]));
+
+        })
     }
-
-    /* display it */ //send log back to server
-
-    songs.forEach(song => {
-        songsLog += "{0} - {1} by {2}. Album: {3} Label: {4}".format(song[COUNT_IDX], song[SONG_IDX], song[ARTIST_IDX], song[RECORD_IDX], song[LABEL_IDX]) + '<br>';
-        //console.log("{0} - {1} by {2}. Album: {3} Label: {4}".format(song[COUNT_IDX], song[SONG_IDX], song[ARTIST_IDX], song[RECORD_IDX], song[LABEL_IDX]));
-
-    })
-
-     }
-     catch(err) {
-        console.log("second time err");
+    catch(err) {
+        console.log(err);
         songsLog = null;
      }
 });
 
-
-
 module.exports = function(filePath, cb) {
+    //open a stream from the uploaded file
     var stream = fs.createReadStream(filePath);
-
     stream.pipe(parser);
     //when the stream is done, songsLog is complete and ready to be returned
     stream.on('close', function() {
--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const express    = require('express');
-const app 	     = express();
-const path       = require('path');
-const port       = process.env.PORT || 3000;
-const formidable = require('formidable');
-const parser     = require('./csvparse.js');
+const express     = require('express');
+const app 	      = express();
+const path        = require('path');
+const port        = process.env.PORT || 3000;
+const formidable  = require('formidable');
+const importFresh = require('import-fresh');
+const fs 		  = require('fs');
 
 //send the index page on a get request
+app.listen(port, () => console.log('Example app listening on port: ' + port));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
-app.listen(port, () => console.log('Example app listening on port: !' + port));
-
-
 
 app.post('/upload', function(req, res) {
+  //load the parser from scratch each time so that the state is reset
+  var parser = importFresh('./csvparse.js');
   //upload the file from the html form
   var form = new formidable.IncomingForm();
   form.parse(req,function(err, fields, files) {
   	if (err) throw err;
   	//get the path to the uploaded file for the parser to use
   	var filePath = files.spinFile.path;
+  	//send the filePath to the parser and do the parsing
   	parser(filePath, function(data) {
-      if (data == null) {
-        res.end();
-      }
-  		res.send("<code>" + data + "</code>");
-      res.end();
+  		//if there is some sort of err, just send the default page again
+      	if (data == null) {
+        	res.sendFile(path.join(__dirname + "/index.html"));
+      	}	
+      		//output the code
+  			res.send("<code>" + data + "</code>");
   	});
   });
 });
-
